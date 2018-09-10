6d42e9f3c8b059de062fe32c807cac96fc36e333
Asaf Davidov <adavidov@conncoll.edu>
2018-09-10 16:34:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
 # Add  code here!
+def prime?(int)
+  if int < 2
+    return false
+  else
+    if int/2 < 2
+      return true
+    else
+      range = Array(2..int/2)
+      current = 0
+      finish = range.size
+      while current < finish
+        if int%range[current]==0 
+          return false
+        end
+        current +=1
+      end
+    end
+    true
+  end
+end
+  
