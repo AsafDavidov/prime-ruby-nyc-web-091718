5c499cbba1e51f3ac59a0f9ca515b7e4bf3dab12
Asaf Davidov <adavidov@conncoll.edu>
2018-09-10 16:27:24 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(int)
+  if int < 2
+    return false
+  else
+    if int/2 < 2
+      return true
+    else
+      range = Array(2..int/2)
+      range.each |num| do if int%num==0 return false
+      end
+    end
+  end
+end
+  
